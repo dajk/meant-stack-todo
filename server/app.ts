095ad9d82b2700e2fb27ff2e5b805188cdb14c28
Radovan Hajdukovic <radovanhajdukovic@Radovans-MBP.home>
2016-10-03 05:15:14 +0200

    Initial commit

---
+++ server/app.ts
@@ -0,0 +1,24 @@
+import * as express from 'express';
+import * as path from 'path';
+
+const app = express();
+const PORT = 1337;
+
+let messages = [
+	{title: 'yes', isDone: true},
+	{title: 'no', isDone: false},
+	{title: 'maybe', isDone: '?'},
+];
+
+app.use('/', express.static(path.join(__dirname, '../client')));
+app.use('/node_modules', express.static(path.join(__dirname, '../node_modules')));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/messages', (req, res) => res.json(messages));
+
+app.listen(PORT, () => {
+	console.log('listening on port: ' + PORT);
+});
