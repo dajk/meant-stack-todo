5e0dfddff18eaf3d42b8e7ea08a2fb560bb039c6
Radovan Hajdukovic <hajdukovic.radovan@gmail.com>
2016-10-10 02:30:24 +0200

    prod env setup

---
+++ config/index.ts
@@ -0,0 +1,16 @@
+export const config = {
+	development: {
+		SRC: '../client',
+		MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017'
+	},
+
+	production: {
+		SRC: '../dist',
+		MONGODB_URI: process.env.MONGODB_URI || ''
+	}
+};
+
+export interface ConfigI {
+	SRC: string;
+	MONGODB_URI: string;
+}
--- server/app.ts
+++ server/app.ts
@@ -4,12 +4,20 @@ import * as mongoose from 'mongoose';
 import { json, urlencoded } from 'body-parser';
 import messageRouter from './routes/message.route';
 import createDefaultMessages from './mock-default-data';
+import { config, ConfigI } from '../config/index';
 
-const MONGOURI: string = 'mongodb://localhost:27017';
+const nodeEnv: string = process.env.NODE_ENV || 'development';
 
-mongoose.connect(MONGOURI + '/meant-stack-todo', (err: Error) => {
+let configEnv: ConfigI;
+
+// Run specific environment
+(nodeEnv !== 'production')
+	?	configEnv = config.development
+	: configEnv = config.production;
+
+mongoose.connect(configEnv.MONGODB_URI + '/meant-stack-todo', (err: Error) => {
 	if (err) return err;
-	console.log('The mongodb has been connected on: ', MONGOURI);
+	console.log('The mongodb has been connected on: ', configEnv.MONGODB_URI);
 });
 
 const app: express.Application = express();
@@ -21,13 +29,13 @@ app.use(urlencoded({ extended: false }));
 // Public folder to serve html (it's looking for index.html by default)
 app.use('/', express.static(path.join(__dirname, '../public')));
 // client path for any Angular 2 compiled script
-app.use('/client', express.static(path.join(__dirname, '../client')));
+app.use('/client', express.static(path.join(__dirname, configEnv.SRC)));
 // Use node modules path as default
 app.use('/node_modules', express.static(path.join(__dirname, '../node_modules')));
 // System.js bundler
 app.use('/', express.static(path.join(__dirname, '../tools')));
 
-// Routers
+// Router
 app.use('/api', messageRouter);
 
 // Listening server on port ${PORT}
